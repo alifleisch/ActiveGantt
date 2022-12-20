f6fbcd98bc9709594d51848e2dec5c7b9fa77fdf
Alexander Kovalchuk <akovalchuk@F24AG-AKOVALCHUK.local>
2022-12-20 22:47:53 +0100

    draft buttons added

--- app/src/main/java/com/example/fitgantt/ui/MainActivity.kt
+++ app/src/main/java/com/example/fitgantt/ui/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fitgantt.ui.theme.FitGanttTheme
 import com.example.fitgantt.viewmodel.MainViewModel
 
@@ -26,22 +31,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Row {
+                        AddActionButton()
+                        Spacer(modifier = Modifier.width(30.dp))
+                        DeleteActionButton()
+                    }
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun AddActionButton(){
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Add action")
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FitGanttTheme {
-        Greeting("Android")
+    @Composable
+    fun DeleteActionButton(){
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Delete action")
+        }
     }
 }
+
